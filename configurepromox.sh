f3229a3ac3226735e1e42006fb6033620a9ed4b0
Daniel Fonseca Alves <dalrafgmail.com>
2017-08-24 13:56:06 -0300

    Adciionado virtio

--- configurepromox.sh
+++ configurepromox.sh
@@ -80,6 +80,7 @@ then
     gunzip pfSense-CE-2.3.4-RELEASE-amd64.iso.gz
     wget -c http://c3sl.dl.osdn.jp/clonezilla/67925/clonezilla-live-2.5.2-17-amd64.iso
     wget -c https://ufpr.dl.sourceforge.net/project/systemrescuecd/sysresccd-x86/5.0.3/systemrescuecd-x86-5.0.3.iso
+    wget -c https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.iso
 fi
 
 
