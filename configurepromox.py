bd9c0184c04546cb3ebe058fff4492b2f2172f5b
Daniel Fonseca Alves <dalraf@gmail.com>
2022-05-31 12:07:00 -0300

    Atualizada versão do ansible

--- configurepromox.py
+++ configurepromox.py
@@ -94,8 +94,7 @@ if (perguntar("Deseja configurar o servidor com ansible") == "y"):
     subprocess.call(["apt-get -y install build-essential libssl-dev libffi-dev python-dev"], shell=True)
     subprocess.call(["apt-get -y install python3-pip"], shell=True)
     subprocess.call(["pip install Jinja2"], shell=True)
-    subprocess.call(["pip install cryptography==2.0.3"], shell=True)
-    subprocess.call(["pip install ansible==2.3.2.0"], shell=True)
+    subprocess.call(["pip install ansible==5.8.0"], shell=True)
     subprocess.call(["ansible-playbook --ask-vault-pass -i localhost --connection=local installproxmox.yml"], shell=True)
 
 if (perguntar("Deseja configurar a raid") == "y"):
