6d579d933ac47408eedbb1fe5296d09dcf5e9013
Daniel Fonseca Alves <dalraf@gmail.com>
2024-03-11 10:19:04 -0300

    Removido pedido de password

--- configurepromox.py
+++ configurepromox.py
@@ -92,7 +92,7 @@ def perguntar(texto):
 if (perguntar("Deseja configurar o servidor com ansible") == "y"):
     subprocess.call(["apt-get -y install build-essential libssl-dev libffi-dev python-dev-is-python3"], shell=True)
     subprocess.call(["apt-get -y install python3-pip ansible"], shell=True)
-    subprocess.call(["ansible-playbook --ask-vault-pass -i localhost --connection=local installproxmox.yml"], shell=True)
+    subprocess.call(["ansible-playbook -i localhost --connection=local installproxmox.yml"], shell=True)
 
 if (perguntar("Deseja configurar a raid") == "y"):
     raid = perguntar("Qual tipo de raid (1, 10, 5-3 ou 5-4)")
